afd6cf65eed332e9c25464cad4a5a2cb6553ea6b
Stefan Majewsky <majewsky@gmx.net>
2025-02-18 23:10:53 +0100

    refined: basic idea

---
+++ refined/refined_test.go
@@ -0,0 +1,58 @@
+/*******************************************************************************
+* Copyright 2025 Stefan Majewsky <majewsky@gmx.net>
+* SPDX-License-Identifier: GPL-3.0-only
+* refined.Refer to the file "LICENSE" for details.
+*******************************************************************************/
+
+package refined_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"regexp"
+	"testing"
+
+	. "github.com/majewsky/gg/internal/test"
+	"github.com/majewsky/gg/refined"
+)
+
+var accountNameRx = regexp.MustCompile(`^[a-z_][a-z0-9_]*$`)
+
+// Full demonstration of a refinement type for the test.
+type AccountName struct {
+	refined.Refined[AccountName, string]
+}
+
+// Demonstration of a struct containing a refinement type.
+type AccountData struct {
+	Name AccountName
+}
+
+func NewAccountName(value string) (AccountName, error) {
+	r, err := refined.Refine[AccountName](value)
+	return AccountName{r}, err
+}
+
+// MatchesValue implements the refined.Condition interface.
+func (AccountName) MatchesValue(value string) error {
+	return refined.RegexpMatch(accountNameRx, value)
+}
+
+// Example for how to access the contained value in computations.
+func (n AccountName) ContainerName() string {
+	return fmt.Sprintf("container-for-%s", n.GetValue())
+}
+
+func TestAccountName(t *testing.T) {
+	buf1 := []byte(`{"Name":"foo"}`)
+	var d1 AccountData
+	err := json.Unmarshal(buf1, &d1)
+	AssertEqual(t, err, error(nil))
+	AssertEqual(t, d1.Name.GetValue(), "foo")
+
+	// TODO: fails because we need specialized unmarshaling logic on type AccountData
+	buf2 := []byte(`{}`)
+	var d2 AccountData
+	err = json.Unmarshal(buf2, &d2)
+	AssertEqual(t, err.Error(), "foo")
+}
