7e539a7d649d8e400bdaad8392fa1d6c07486376
Stefan Majewsky <majewsky@gmx.net>
2025-04-24 10:56:48 +0200

    add options.Max(), options.Min()

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# v1.1.0 (TBD)
+
+Changes:
+
+- Add `options.Max()` and `options.Min()`.
+
 # v1.0.0 (2025-02-12)
 
 Initial release. The Go version requirement is 1.24.0 because `type Option`
--- options/options_test.go
+++ options/options_test.go
@@ -29,3 +29,43 @@ func TestMap(t *testing.T) {
 	AssertEqual(t, Map(None[int](), strconv.Itoa), None[string]())
 	AssertEqual(t, Map(Some(42), strconv.Itoa), Some("42"))
 }
+
+func TestMax(t *testing.T) {
+	AssertEqual(t, Max[int](), None[int]())
+	AssertEqual(t, Max(None[int]()), None[int]())
+	AssertEqual(t, Max(None[int](), None[int]()), None[int]())
+
+	AssertEqual(t, Max(Some(5)), Some(5))
+	AssertEqual(t, Max(Some(5), None[int]()), Some(5))
+	AssertEqual(t, Max(None[int](), Some(5)), Some(5))
+
+	AssertEqual(t, Max(Some(5), Some(23)), Some(23))
+	AssertEqual(t, Max(None[int](), Some(5), Some(23)), Some(23))
+	AssertEqual(t, Max(Some(5), None[int](), Some(23)), Some(23))
+	AssertEqual(t, Max(Some(5), Some(23), None[int]()), Some(23))
+
+	AssertEqual(t, Max(Some(23), Some(5)), Some(23))
+	AssertEqual(t, Max(None[int](), Some(23), Some(5)), Some(23))
+	AssertEqual(t, Max(Some(23), None[int](), Some(5)), Some(23))
+	AssertEqual(t, Max(Some(23), Some(5), None[int]()), Some(23))
+}
+
+func TestMin(t *testing.T) {
+	AssertEqual(t, Min[int](), None[int]())
+	AssertEqual(t, Min(None[int]()), None[int]())
+	AssertEqual(t, Min(None[int](), None[int]()), None[int]())
+
+	AssertEqual(t, Min(Some(5)), Some(5))
+	AssertEqual(t, Min(Some(5), None[int]()), Some(5))
+	AssertEqual(t, Min(None[int](), Some(5)), Some(5))
+
+	AssertEqual(t, Min(Some(5), Some(23)), Some(5))
+	AssertEqual(t, Min(None[int](), Some(5), Some(23)), Some(5))
+	AssertEqual(t, Min(Some(5), None[int](), Some(23)), Some(5))
+	AssertEqual(t, Min(Some(5), Some(23), None[int]()), Some(5))
+
+	AssertEqual(t, Min(Some(23), Some(5)), Some(5))
+	AssertEqual(t, Min(None[int](), Some(23), Some(5)), Some(5))
+	AssertEqual(t, Min(Some(23), None[int](), Some(5)), Some(5))
+	AssertEqual(t, Min(Some(23), Some(5), None[int]()), Some(5))
+}
